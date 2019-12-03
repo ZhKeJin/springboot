ba195e042b03a2b4735039ee84fe64edf7fc7e2a
ZhKeJin <3280221076@qq.com>
2019-12-03 21:06:51 +0800

    springboot_1

---
+++ springboot_1/src/main/java/com/zhang/springboot/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.zhang.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Zhangkj
+ * @date 2019-12-03-19:57
+ */
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/world")
+    @ResponseBody
+    public String hello(){
+        return "springboot.....";
+    }
+
+    @RequestMapping("/boot/index")
+    public String hello_1(){
+        return "index";
+    }
+}
